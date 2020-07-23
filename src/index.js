0cd24d2d1d9dc6fe6b044c473a9b2bb8d9be886f
Gustavo Delgadillo <Gustavo.Delgadillo@fresenius-netcare.com>
2020-07-23 17:35:12 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Header name="World!!"></Header>
+    <Menu></Menu>
+    <Content></Content>
+    <Footer></Footer>
+ </React.StrictMode>,
   document.getElementById('root')
 );
 

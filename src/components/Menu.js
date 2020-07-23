c0dcb83df9cb5f44f3f345dcb906a926078a9a3c
Gustavo Delgadillo <Gustavo.Delgadillo@fresenius-netcare.com>
2020-07-23 18:23:35 -0500

    Initial Commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,9 +8,9 @@ export default class Menu extends React.Component{
         <div className="Menu">
         <nav>
             <ul>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>History</a></li>
-                <li><a href='#'>Contact</a></li>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/'>History</a></li>
+                <li><a href='/'>Contact</a></li>
                 </ul>
         </nav>
         </div>
